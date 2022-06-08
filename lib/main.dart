6211a8d1b913f721595b3f76134def5a4586764b
PintaPontus <mateo.blascoviz@gmail.com>
2022-06-08 19:11:58 +0200

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  setupFirebase();
   runApp(const MyApp());
+}
+
+Future<void> setupFirebase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
